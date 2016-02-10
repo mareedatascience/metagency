8307ae4a4dabfb7d85568ba5c312d3fd1dd25f52
mareedatascience <mareedatascience@gmail.com>
2016-02-11 09:10:23 +1100

    More rules for station

--- station/urls.py
+++ station/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^([0-9]{4,6})/(.*)', views.wx, name='wx'),
-	url(r'^([0-9]{4,6})$', views.station, name='station'),
+	url(r'^search/(.+)/([html|json|csv|pdf]*)', views.search, name='search'),
+	url(r'^search', views.searchpage, name='searchpage'),
+	url(r'^([0-9]{4,6})/(.+)/(.+)/([html|json|csv|pdf]*)', views.wx, name='fx'),
+	url(r'^([0-9]{4,6})/(.+)/([html|json|csv|pdf]*)', views.wx, name='wx'),
+	url(r'^([0-9]{4,6})/([html|json|csv|pdf]*)$', views.station, name='station'),
     url(r'^$', views.index, name='index'),
 ]
--- station/views.py
+++ station/views.py
@@ -4,9 +4,18 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("This is a list of stations (with pagination)")
 
-def station(request, stn_num):
-	return HttpResponse("This is a page for {stn_num}. It includes station metadata, a list of nearby stations, station climatology, list of products available".format(stn_num=stn_num))
+def station(request, stn_num, fmt="html"):
+	return HttpResponse("This is a page for {stn_num} in {fmt} format. It includes station metadata, a list of nearby stations, station climatology, list of products available".format(stn_num=stn_num, fmt=fmt))
 
-def wx(request, stn_num, wx):
-	return HttpResponse("This is a page for {stn_num} and weather element {wx}. It has a list of reporting frequencies available or the product itself in the default format".format(stn_num=stn_num, wx=wx))
+def wx(request, stn_num, wx, fmt="html"):
+	return HttpResponse("This is a page for {stn_num} and weather element {wx} in {fmt} format. It has a list of reporting frequencies available or the product itself in the default format".format(stn_num=stn_num, wx=wx, fmt=fmt))
+
+def fx(request, stn_num, wx, fx, fmt="html"):
+	return HttpResponse("This is a page for {stn_num} and weether element {wx} at reporting frequency {wx} in {fmt} format.".format(stn_num=stn_num, wx=wx, fx=fx, fmt=fmt))
+
+def search(request, query, fmt="html"):
+	return HttpResponse("This is a page to display search results for stations matching {query} displayed in {fmt} format".format(query=query, fmt=fmt))
+
+def searchpage(request):
+	return HttpResponse("This is a html page with a UI for station searches")
 
